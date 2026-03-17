39d0f7b5b2ecf1025124d6a168c291b28780f64d
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-03-17 19:03:43 -0300

    feat: class Persona

---
+++ tp1/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+    private String nombre;
+    private int dni;
+
+    public Persona(String nombre, int dni){
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+}
