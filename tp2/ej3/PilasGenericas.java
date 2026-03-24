c1fb6bb263e4f85ee9c93e1df000ea931809fa86
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-03-24 17:01:29 -0300

    ej3

--- tp2/ej3/PilasGenericas.java
+++ tp2/ej3/PilasGenericas.java
@@ -4,9 +4,14 @@ import java.util.Stack;
 
 public class PilasGenericas<E> {
 
+    private int mayorSize(Stack<E> p1, Stack<E>p2){
+        if (p1.size() > p2.size())
+            return p1.size();
+        return p2.size();
+    }
     public Stack<E> intercalar(Stack<E> p1, Stack<E>p2){
         Stack<E> res = new Stack<E>();
-        int suma = p1.size() + p2.size();
+        int suma = mayorSize(p1, p2);
         for (int i = 0; i < suma; i++){
             if (!p1.empty()){
                 res.push(p1.pop());
