0da75102b5ce5a69f6003d27e6ea2d1e60eacbdd
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-03-30 16:58:14 -0300

    ej4 (dudas)

--- tp3/ej4/ColaConPila.java
+++ tp3/ej4/ColaConPila.java
@@ -1,3 +1,20 @@
-public class ColaConPila{
-    
+import java.util.Stack;
+public class ColaConPila<E> implements Queue<E>{
+    protected Stack<E> pila = new Stack<E>();
+
+    public int size(){
+        return pila.size(); 
+    }
+    public boolean isEmpty(){
+        return pila.isEmpty();
+    }
+    public E front(){
+        // ?
+    }
+    public void enqueue(E element){
+        pila.add(element); // ?????
+    }
+    public E dequeue(){
+        // ??????
+    }
 }
