4aa75566771bdaf7265592cb3c617f9af4843a37
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-03-30 16:30:33 -0300

    isEmpty, EmptyQeueException

--- tp3/ej2/ColaArreglo.java
+++ tp3/ej2/ColaArreglo.java
@@ -1,10 +1,17 @@
 public class ColaArreglo implements Queue{
     protected int f;
     protected int r;
+    protected int tamanio;
 
     public ColaArreglo(int t){
         f = 0;
         r = 0;
+        tamanio = 0;
+    }
+
+    public boolean isEmpty(){
+        return tamanio == 0;
+        // antes: f == r
     }
     
 }
