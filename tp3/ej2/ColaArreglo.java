1d51bbb25b8287ec10b1a0b120f198e174ff8a01
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-03-30 16:26:42 -0300

    queue constructor

--- tp3/ej2/ColaArreglo.java
+++ tp3/ej2/ColaArreglo.java
@@ -1,3 +1,10 @@
-public class ColaArreglo {
+public class ColaArreglo implements Queue{
+    protected int f;
+    protected int r;
+
+    public ColaArreglo(int t){
+        f = 0;
+        r = 0;
+    }
     
 }
