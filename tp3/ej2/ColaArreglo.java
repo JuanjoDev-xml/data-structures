0e4ea79ed88189fdc6be47a035669c676f0e0084
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-03-30 16:46:50 -0300

    ej2

--- tp3/ej2/ColaArreglo.java
+++ tp3/ej2/ColaArreglo.java
@@ -1,7 +1,8 @@
-public class ColaArreglo implements Queue{
+public class ColaArreglo<E> implements Queue<E>{
     protected int f;
     protected int r;
     protected int tamanio;
+    protected E [] q;
 
     public ColaArreglo(int t){
         f = 0;
@@ -13,5 +14,30 @@ public class ColaArreglo implements Queue{
         return tamanio == 0;
         // antes: f == r
     }
-    
+    public E front() throws EmptyQueueException{
+        if (isEmpty()){
+            throw new EmptyQueueException("Cola vacía");
+        }
+        return q[f];
+    }
+    public int size(){
+        return tamanio;
+    }
+    public void enqueue(E element) throws FullQueueException{
+        if (size() == tamanio){
+            throw new FullQueueException("Cola llena");
+        }
+        q[r] = element;
+        r = (r+1) % (tamanio+1);
+        // por qué no simplemente r++ ??????
+    }
+    public E dequeue() throws EmptyQueueException{
+        if (isEmpty()){
+            throw new EmptyQueueException("Cola vacía");
+        }
+        E res = q[f];
+        q[f] = null;
+        f = (f+1) / (tamanio+1); // ??????????????????????
+        return res;
+    }
 }
