1c7c4ab7c1f97b85ae0660506e6a71155b493f12
Juan José Denk Guido <juanjosedenk05@gmail.com>
2026-06-25 16:06:42 -0300

    feat: ElementIterator setup

---
+++ tp4/ElementIterator.java
@@ -0,0 +1,11 @@
+package ar.edu.uns.cs.ed.tdas.tps.tp4.ej1;
+
+import ar.edu.uns.cs.ed.tdas.tdalista.PositionList;
+import ar.edu.uns.cs.ed.tdas.Position;
+
+import java.util.*;
+public class ElementIterator<E> implements Iterator <E> {
+    protected PositionList<E> lista; // Lista a iterar
+    protected Position<E> cursor; // Posición del elemento corriente
+    
+}
