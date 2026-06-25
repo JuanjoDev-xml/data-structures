da9829d6b8a9402e6b8a528fe4c591378a9327f0
Juan José Denk Guido <juanjosedenk05@gmail.com>
2026-06-25 16:07:37 -0300

    Implement constructor for ElementIterator
    Added constructor to initialize ElementIterator with a PositionList.

--- tp4/ElementIterator.java
+++ tp4/ElementIterator.java
@@ -5,7 +5,13 @@ import ar.edu.uns.cs.ed.tdas.Position;
 
 import java.util.*;
 public class ElementIterator<E> implements Iterator <E> {
+    // Atributos de instancia
     protected PositionList<E> lista; // Lista a iterar
     protected Position<E> cursor; // Posición del elemento corriente
-    
+    // Constructores
+    public ElementIterator (PositionList <E> l ) {
+        lista = l; // Guardo la lista a iterar
+        if (lista.isEmpty()) cursor = null; // Si la lista está vacía, la posición corriente es nula
+        else cursor = lista.first(); // Sino la posición corriente es la primera de la lista
+    }
 }
