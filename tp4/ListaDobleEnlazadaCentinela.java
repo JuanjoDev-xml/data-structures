91601e8a6451fd521054b526a1c37f31ad952a94
Juan José Denk Guido <juanjosedenk05@gmail.com>
2026-07-06 17:52:22 -0300

    feat: ListaDobleEnlazadaCentinela setup
    Implement initial structure for doubly linked sentinel list.

---
+++ tp4/ListaDobleEnlazadaCentinela.java
@@ -0,0 +1,13 @@
+package ar.edu.uns.cs.ed.tdas.tps.tp4.ej1;
+
+import ar.edu.uns.cs.ed.tdas.excepciones.BoundaryViolationException;
+import ar.edu.uns.cs.ed.tdas.excepciones.EmptyListException;
+import ar.edu.uns.cs.ed.tdas.excepciones.InvalidPositionException;
+import ar.edu.uns.cs.ed.tdas.tdalista.PositionList;
+import ar.edu.uns.cs.ed.tdas.Position;
+
+import java.util.Iterator;
+
+public class ListaDobleEnlazadaCentinela<E> implements PositionList<E>{
+   
+}
